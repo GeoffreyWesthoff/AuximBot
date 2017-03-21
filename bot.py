a21e445ffb6a55001498a8e121dd546cf3b5cc4f
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2017-03-21 18:38:36 +0100

    Added now playing status and autoremove after 12 hours

--- bot.py
+++ bot.py
@@ -22,7 +22,6 @@ async def on_reddit():
             stickied = 'Er is een nieuwe sticky thread: ' + str(submission.title) + ' ' + str(submission.url)
     return stickied
 
-
 @client.event
 async def on_ready():
     global loggedin
@@ -33,10 +32,20 @@ async def on_ready():
     titel = await on_reddit()
     if titel:
         await client.send_message(client.get_channel('ANNOUNCEMENT_CHANNEL'), titel)
+        await delete_annoucements()
     amatext= 'ama'
     if amatext in titel:
         await client.send_message(client.get_channel('ANNOUNCEMENT_CHANNEL'),'@everyone')
+    await client.change_presence(game=discord.Game(name='mygame'))
     await asyncio.sleep(300)
     await on_ready()
+
+async def delete_annoucements():
+    await asyncio.sleep(43200)
+    await client.purge_from(client.get_channel('ANNOUNCEMENT_CHANNEL'), limit=5, check=is_me)
+    print('Cleared bot messages')
+
+def is_me(m):
+    return m.author == client.user
 
 client.run('DISCORDBOT_TOKEN')
